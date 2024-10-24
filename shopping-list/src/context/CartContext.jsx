920b8f513da9811dea9f5fc5c3ce8e77fece369e
muthana-abo-alez1 <92632582+muthana-abo-alez1@users.noreply.github.com>
2024-10-24 20:36:07 +0300

    add cart context

---
+++ shopping-list/src/context/CartContext.jsx
@@ -0,0 +1,46 @@
+import React, { createContext, useState, useContext } from "react";
+
+const CartContext = createContext();
+
+export const useCart = () => useContext(CartContext);
+
+export const CartProvider = ({ children }) => {
+  const [cartItems, setCartItems] = useState([]);
+
+  const addToCart = (product, quantity) => {
+    setCartItems((prevItems) => {
+      const itemExists = prevItems.find((item) => item.id === product.id);
+      if (itemExists) {
+        return prevItems.map((item) =>
+          item.id === product.id
+            ? { ...item, quantity: item.quantity + quantity } 
+            : item
+        );
+      } else {
+        return [...prevItems, { ...product, quantity }]; 
+      }
+    });
+  };
+
+  const updateQuantity = (id, quantity) => {
+    setCartItems((prevItems) => 
+      prevItems.map((item) =>
+        item.id === id ? { ...item, quantity } : item
+      )
+    );
+  };
+
+  const removeFromCart = (id) => {
+    setCartItems((prevItems) => prevItems.filter((item) => item.id !== id));
+  };
+
+  const getTotal = () => {
+    return cartItems.reduce((total, item) => total + item.price * item.quantity, 0).toFixed(2);
+  };
+
+  return (
+    <CartContext.Provider value={{ cartItems, addToCart, removeFromCart, updateQuantity, getTotal ,setCartItems}}>
+      {children}
+    </CartContext.Provider>
+  );
+};
