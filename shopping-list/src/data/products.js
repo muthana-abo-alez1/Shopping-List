cf836f5879dc899c573633885db6f5f10ef38255
muthana-abo-alez1 <92632582+muthana-abo-alez1@users.noreply.github.com>
2024-10-24 20:36:40 +0300

    add mock data (products)

---
+++ shopping-list/src/data/products.js
@@ -0,0 +1,65 @@
+const products = [
+  {
+    "id": 1,
+    "name": "Comfortable Wear",
+    "price": "70",
+    "img": "https://i.ebayimg.com/images/g/1eQAAOSw9Y9iCiXO/s-l1200.jpg"
+  },
+  {
+    "id": 2,
+    "name": "Elegant Shoes",
+    "price": "90",
+    "img": "https://baccabucci.com/cdn/shop/products/MG_7666-min_1e99cd5a-e357-41a4-8048-56df9c9b30cc.jpg?v=1681747325"
+  },
+  {
+    "id": 3,
+    "name": "Cushioned Sneakers",
+    "price": "110",
+    "img": "https://japan-outfits.com/cdn/shop/products/sporty-aesthetic-chunky-sole-sne.webp?v=1679316988"
+  },
+  {
+    "id": 4,
+    "name": "Trendy Boots",
+    "price": "60",
+    "img": "https://d1nymbkeomeoqg.cloudfront.net/photos/25/44/375879_18149_XL.jpg"
+  },
+  {
+    "id": 5,
+    "name": "Modern Loafers",
+    "price": "150",
+    "img": "https://www.civardi-shoes.com/wp-content/uploads/2022/07/custom-casual-suede-slip-on-loafers-JIM-scaled.jpg"
+  },
+  {
+    "id": 6,
+    "name": "Adjustable Sandals",
+    "price": "55",
+    "img": "https://img.ltwebstatic.com/images3_pi/2023/04/26/1682503481a0589e77eadc9e550f01e9db3e64b61a_thumbnail_720x.jpg"
+  },
+  {
+    "id": 7,
+    "name": "Soft Flats",
+    "price": "65",
+    "img": "https://1861.ca/cdn/shop/products/maree-blush-9.jpg?v=1663789205&width=2048"
+  },
+  {
+    "id": 8,
+    "name": "Leather Boots",
+    "price": "85",
+    "img": "https://www.cnet.com/a/img/resize/bf54f5fc38dd9239b6453aa521103192b82ebfac/hub/2023/03/28/61964c47-818d-4ee6-b6cf-62165fbc1bf2/img-6871.jpg?auto=webp&fit=crop&height=576&width=768"
+  },
+  {
+    "id": 9,
+    "name": "Retro Sneakers",
+    "price": "100",
+    "img": "https://shoezero.com/cdn/shop/articles/cassandra.h_high_top_sneakers_beige_and_brown_and_red_and_green_2c075e8f-d027-49b3-927d-85d5ac76a57d.webp?v=1699227274&width=2048"
+  },
+  {
+    "id": 10,
+    "name": "Comfort Slip-ons",
+    "price": "110",
+    "img": "https://people.com/thmb/U1FORO-f0UqDCIue1qpJW_dHT7c=/fit-in/1500x2568/filters:no_upscale():max_bytes(150000):strip_icc()/peo-slip-on-shoes-test-olukai-pehuea-womens-sneakers-taylor-fox-03-3a182eb4355b47dbb4094ac8d3216f0e.jpg"
+  }
+]
+
+  export default products;
+  
